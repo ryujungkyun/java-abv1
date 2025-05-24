f8715a417568a015cb2db175f18d354cb9ee9c15
lyujeong-gyun <prank01@naver.com>
2025-05-25 01:09:11 +0900

    java-abv1

---
+++ src/thread/start/test/StartTest4Main.java
@@ -0,0 +1,38 @@
+package thread.start.test;
+
+import javax.swing.text.TabableView;
+
+import static util.MyLogger.log;
+
+public class StartTest4Main {
+
+    public static void main(String[] args) {
+
+        new Thread(new Printer("A", 1000), "Thread-A").start();
+        new Thread(new Printer("B", 500), "Thread-B").start();
+
+    }
+
+    static class Printer implements Runnable {
+
+        private String content;
+        private int sleepMs;
+
+        public Printer(String content, int sleepMs) {
+            this.content = content;
+            this.sleepMs = sleepMs;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                log(content);
+                try {
+                    Thread.sleep(sleepMs);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+}
